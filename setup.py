46afd4bbc74b2752cc139a0db8cfc34b0aca1d18
GianPonzi <giancarlodolacioponzi@gmail.com>
2023-07-18 22:09:20 -0300

    dataingest2

--- setup.py
+++ setup.py
@@ -1,25 +1,23 @@
-from setuptools import find_packages, setup
+from setuptools import find_packages,setup
 from typing import List
 
-
-hypen_e_dot='-e .'
-
-def get_requirements(file_path:str)-> List[str]:
+HYPEN_E_DOT='-e .'
+def get_requirements(file_path:str)->List[str]:
     '''
     this function will return the list of requirements
     '''
     requirements=[]
     with open(file_path) as file_obj:
         requirements=file_obj.readlines()
-        [req.replace("\n","") for req in requirements]
+        requirements=[req.replace("\n","") for req in requirements]
 
-        if hypen_e_dot in requirements:
-            requirements.remove(hypen_e_dot)
-
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+    
     return requirements
 
 setup(
-name='ML_Project',
+name='mlproject',
 version='0.0.1',
 author='Gian',
 author_email='giancarlodolacioponzi@gmail.com',
